e25e91a7ec9cac116f47cdb53c187282f3262469
zulfahmidev <developer.zulfahmi@gmail.com>
2023-04-02 01:54:42 +0700

    Memperbaiki tampilan

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="h-screen flex items-center justify-center bg-slate-200">
-    <div class="w-96 shadow-lg p-4 bg-slate-50 rounded-md border-t-4 border-yellow-300">
-      <h1 class="text-xl">Notes</h1>
-      <div class="w-full py-2 rounded-md bg-slate-100 mt-2 border-t-2 border-slate-300 flex justify-between">
-        <div class="px-4">
-          test
+    <div class="w-96 h-96 shadow-lg p-4 bg-slate-50 rounded-md border-t-4 border-yellow-300">
+      <div class="flex justify-between items-center ">
+        <h1 class="text-xl">Notes</h1>
+        <div class="rounded-full active:bg-slate-200 bg-slate-100 text-slate-800 cursor-pointer w-7 h-7 flex items-center justify-center">
+          <Icon name="material-symbols:add-circle-outline" />
+        </div>
+      </div>
+      <div class="w-full py-2 rounded-md bg-slate-100 mt-2 hover:shadow-md border-t-2 border-slate-300 flex justify-between items-center" v-for="index in 5" :key="index">
+        <div class="px-4 cursor-pointer" @click="navigateTo(`note-${index}`)">
+          note-{{ index }}
         </div>
         <div class="pr-4">
           <div class="rounded-full active:bg-slate-200 text-slate-800 cursor-pointer w-7 h-7 flex items-center justify-center">
---
+++ pages/note-[id].vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="h-screen flex items-center justify-center bg-slate-200">
+    <div class="w-96 h-96 shadow-lg p-4 bg-slate-50 rounded-md border-t-4 border-yellow-300">
+      <div class="flex items-center cursor-pointer text-slate-500 hover:text-slate-950" @click="navigateTo({name: 'index'})">
+        <Icon name="material-symbols:arrow-back-ios-new" />
+        <h1 class="text-sm ml-2">Back</h1>
+      </div>
+      <div class="bg-slate-100 h-80 rounded-md mt-2 overflow-auto px-4 py-2">
+        <div class="border-b py-1 text-slate-950">
+          Example Note
+        </div>
+        <p class="text-slate-600 text-sm my-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto vel veritatis officiis quae ducimus quibusdam suscipit asperiores numquam consequatur doloribus, quo reiciendis tenetur ab, veniam omnis eveniet a, culpa dolorum molestias! Aspernatur tempora ab adipisci numquam qui delectus, dolore quaerat, alias veritatis ipsum magnam? Voluptas eos, tempora quibusdam ipsum exercitationem ipsam. Exercitationem minima voluptatibus dolores cum atque facere quis sequi ipsum? Asperiores natus ut odit itaque expedita, dolorum debitis repellat vel, officiis sunt quibusdam cum, totam nemo nisi dicta aliquam impedit! Iste expedita consequuntur sunt quia magni dolores ex laudantium repellendus. Nesciunt in temporibus quae magni fugiat est ex quidem!
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
