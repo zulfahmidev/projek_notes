9f1e1787b3ae264a9341a5102940a739cb008eae
zulfahmidev <developer.zulfahmi@gmail.com>
2023-04-02 00:47:57 +0700

    First Commit

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <!-- <h1>Hello World</h1>
+    <AppAlert>
+      This is an alert
+    </AppAlert> -->
+    <!-- <NuxtWelcome /> -->
+    <NuxtPage></NuxtPage>
+  </div>
+</template>
+
+<script setup>
+  const config = useRuntimeConfig()
+</script>
---
+++ pages/note.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="h-screen flex items-center justify-center bg-slate-200">
+    <div class="w-96 shadow-lg p-4 bg-slate-50 rounded-md border-t-4 border-yellow-300">
+      <h1 class="text-xl">Notes</h1>
+      <div class="w-full py-2 rounded-md bg-slate-100 mt-2 border-t-2 border-slate-300 flex justify-between">
+        <div class="px-4">
+          test
+        </div>
+        <div class="pr-4">
+          
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
